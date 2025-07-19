4fbc4b547ebf337fa9b63f7f00d9d99e1bc4df86
ibokaan <ibrahimkaanatlamis@gmail.com>
2025-07-19 06:11:24 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/timedecode/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.timedecode
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: Text(
+          'TimeDecode',
+          style: TextStyle(
+            fontSize: 36,
+            fontWeight: FontWeight.bold,
+            color: Colors.deepPurple,
+          ),
+        ),
+      ),
+    ),
+  ));
+}
