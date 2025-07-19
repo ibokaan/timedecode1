b771d8334f2c9860ac178eef5722f05bfc4ecae9
ibokaan <ibrahimkaanatlamis@gmail.com>
2025-07-19 06:16:19 +0300

    deneme1

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,22 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MaterialApp(
     home: Scaffold(
-      body: Center(
-        child: Text(
-          'TimeDecode',
-          style: TextStyle(
-            fontSize: 36,
-            fontWeight: FontWeight.bold,
-            color: Colors.deepPurple,
+      backgroundColor: Colors.black, // Arka planı siyah yap
+      body: Padding(
+        padding: EdgeInsets.only(top: 50), // Yukarıdan boşluk (başlık gibi)
+        child: Align(
+          alignment: Alignment.topCenter, // Yazıyı yukarı ortala
+          child: Text(
+            'TimeDecode',
+            style: TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+              color: Colors.white, // Yazı rengi beyaz
+            ),
           ),
         ),
       ),
     ),
   ));
 }
+
